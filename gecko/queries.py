1ea9e28fccbce3b48479df6e6b27e8bec0f20d3b
Rafael <rafael@resmio.com>
2015-07-29 16:34:01 +0200

    Added number of bookings.

--- gecko/queries.py
+++ gecko/queries.py
@@ -1,10 +1,19 @@
 QUERIES = {
 
-    'facilities':
+    'number_facilities':
 
     """
-        SELECT * FROM bookoya_facility AS f
+        SELECT * FROM bookoya_facility f
         WHERE f.verified IS TRUE
 
+    """,
+
+    'number_bookings':
+
     """
+        SELECT b.created FROM bookoya_booking b
+        JOIN bookoya_facility f ON f.id = b.facility_id
+        WHERE f.verified IS TRUE
+
+    """,
 }
--- gecko/views.py
+++ gecko/views.py
@@ -12,11 +12,20 @@ geckoboard = Geckoboard(app)
 @app.route('/number_facilities')
 @geckoboard.number
 def number_facilities():
-    facilities = run_query('facilities')
-    facilities['date_created'] = map(lambda d: d.date(),
-                                     facilities.created)
+    facilities = run_query('number_facilities')
+    facilities.created = facilities.created.apply(lambda d: d.date())
     last_month = facilities[
-        facilities.date_created < (
-            date.today() - timedelta(days=30))].id.count()
+        facilities.created < (date.today() - timedelta(days=30))].id.count()
     this_month = facilities.id.count()
+    return (this_month, last_month)
+
+
+@app.route('/number_bookings')
+@geckoboard.number
+def number_bookings():
+    bookings = run_query('number_bookings')
+    bookings.created = bookings.created.apply(lambda d: d.date())
+    last_month = bookings[
+        bookings.created < (date.today() - timedelta(days=30))].created.count()
+    this_month = bookings.created.count()
     return (this_month, last_month)
