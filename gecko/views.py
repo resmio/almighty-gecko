e5ec3c6933fad39742e30768702ff3f172c128a5
Rafael <rafael@resmio.com>
2015-07-29 14:35:41 +0200

    Add a view to test number of facilities.

--- gecko/views.py
+++ gecko/views.py
@@ -1,11 +1,22 @@
+from datetime import date, timedelta
+
+import pandas as pd
 from flask import Flask
 from flask_geckoboard import Geckoboard
+
+from query_db import run_query
 
 app = Flask(__name__)
 geckoboard = Geckoboard(app)
 
 
-@app.route('/test')
+@app.route('/number_facilities')
 @geckoboard.number
-def test():
-    return (123, 1234)
+def number_facilities():
+    fname = run_query('facilities')
+    facilities = pd.read_csv(fname)
+    last_month = facilities[
+        facilities.date_created < (
+            date.today() - timedelta(days=30))].id.count()
+    this_month = facilities.id.count()
+    return (this_month, last_month)
