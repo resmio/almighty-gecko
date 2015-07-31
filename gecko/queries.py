8a9fdfaafd03ece8c254bb2a0fcfff169a429b68
Rafael <rafael@resmio.com>
2015-07-31 13:47:19 +0200

    Update active_facilities query.

--- gecko/queries.py
+++ gecko/queries.py
@@ -20,8 +20,9 @@ QUERIES = {
     'active_facilities':
 
     """
-        SELECT b.created, f.id AS facility_id FROM bookoya_booking b
-        JOIN bookoya_facility f ON f.id = b.facility_id
+        SELECT b.created, f.id AS facility_id, f.created AS f_created
+        FROM bookoya_booking b
+        FULL OUTER JOIN bookoya_facility f ON f.id = b.facility_id
         WHERE f.verified IS TRUE
 
     """,
