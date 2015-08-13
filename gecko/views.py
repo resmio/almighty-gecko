f5b51415a8516314c55a96bb4d2f99295e3e3fed
Rafael <rafael@resmio.com>
2015-08-13 14:26:55 +0200

    Added weekly overview of actived/deactivated/reactived facilities.

--- gecko/views.py
+++ gecko/views.py
@@ -56,3 +56,44 @@ def active_verified_facilities():
     return {'series': [{'data': actives[::-1], 'name': 'Active'},
                        {'data': verifieds[::-1], 'name': 'Verified'}],
             'x_axis': {'labels': dates[::-1], 'type': 'datetime'}}
+
+
+@app.route('/new_lost_active_facilities')
+@geckoboard.line_chart
+def new_lost_active_facilities():
+    df = run_query('active_verified_facilities')
+    bookings = df[df.created.notnull()]
+    bookings.created = bookings.created.apply(lambda d: d.date())
+    df.f_created = df.f_created.apply(lambda d: d.date())
+    today = date.today().toordinal()
+    # Get the last sunday
+    current_date = date.fromordinal(today - (today % 7))
+    end_date = date(day=1, month=1, year=2015)
+    delta = timedelta(days=7)
+    dates = []
+    active = []
+    deactive = []
+    reactive = []
+    while current_date >= end_date:
+        dates.append('{}'.format(current_date))
+        this_month = set(bookings[
+            (bookings.created <= current_date) &
+            (bookings.created >= (current_date - timedelta(days=30)))]
+            .facility_id.unique())
+        last_month = set(bookings[
+            (bookings.created >= (current_date - timedelta(days=60))) &
+            (bookings.created < (current_date - timedelta(days=30)))]
+            .facility_id.unique())
+        before = set(bookings[
+            bookings.created <= (current_date - timedelta(days=60))]
+            .facility_id.unique())
+        active.append(len(this_month.difference(
+            last_month.union(before))))
+        deactive.append(len(last_month.difference(this_month)))
+        reactive.append(len(before.difference(
+            last_month).intersection(this_month)))
+        current_date -= delta
+    return {'series': [{'data': active[::-1], 'name': 'Activated'},
+                       {'data': deactive[::-1], 'name': 'De-activated'},
+                       {'data': reactive[::-1], 'name': 'Re-activated'}],
+            'x_axis': {'labels': dates[::-1], 'type': 'datetime'}}
