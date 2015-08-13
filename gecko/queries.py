6d216544cf02322361f33dc753857dbe181158cd
Rafael <rafael@resmio.com>
2015-08-13 16:15:42 +0200

    Remove queries.

--- gecko/queries.py
+++ gecko/queries.py
@@ -1,22 +1,4 @@
 QUERIES = {
-
-    'number_facilities':
-
-    """
-        SELECT * FROM bookoya_facility f
-        WHERE f.verified IS TRUE
-
-    """,
-
-    'number_bookings':
-
-    """
-        SELECT b.created FROM bookoya_booking b
-        JOIN bookoya_facility f ON f.id = b.facility_id
-        WHERE f.verified IS TRUE
-
-    """,
-
     'active_verified_facilities':
 
     """
