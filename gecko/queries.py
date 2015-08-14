64669de02e38d48358250d989e0b2ac78408df4f
Rafael <rafael@resmio.com>
2015-08-14 10:10:33 +0200

    Update queries.

--- gecko/queries.py
+++ gecko/queries.py
@@ -1,5 +1,5 @@
 QUERIES = {
-    'active_verified_facilities':
+    'bookings_and_facilities':
 
     """
         SELECT b.created, f.id AS facility_id, f.created AS f_created
@@ -9,12 +9,21 @@ QUERIES = {
 
     """,
 
+    'bookings':
+
+    """
+        SELECT b.created, b.facility_id FROM bookoya_booking b
+        JOIN bookoya_facility f ON f.id = b.facility_id
+        WHERE f.verified IS TRUE AND date_part('year', b.created) = 2015
+
+    """,
+
     'bookings_with_subscription':
 
     """
-        SELECT b.created, f.id AS facility_id, s.subscription_type
+        SELECT b.created, b.facility_id, s.subscription_type
         FROM bookoya_booking b
-        FULL OUTER JOIN bookoya_facility f ON f.id = b.facility_id
+        JOIN bookoya_facility f ON f.id = b.facility_id
         INNER JOIN bookoya_monthlysubscription s ON b.facility_id = s.facility_id
         WHERE f.verified IS TRUE AND date_part('year', b.created) = 2015
 
