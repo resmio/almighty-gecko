f2e19f44d78f8338ee30231c13cf52c7be8a5c05
Rafael <rafael@resmio.com>
2015-08-14 11:59:00 +0200

    Remove 2015 contraint from queries.

--- gecko/queries.py
+++ gecko/queries.py
@@ -5,7 +5,7 @@ QUERIES = {
         SELECT b.created, f.id AS facility_id, f.created AS f_created
         FROM bookoya_booking b
         FULL OUTER JOIN bookoya_facility f ON f.id = b.facility_id
-        WHERE f.verified IS TRUE AND date_part('year', b.created) = 2015
+        WHERE f.verified IS TRUE
 
     """,
 
@@ -14,7 +14,7 @@ QUERIES = {
     """
         SELECT b.created, b.facility_id FROM bookoya_booking b
         JOIN bookoya_facility f ON f.id = b.facility_id
-        WHERE f.verified IS TRUE AND date_part('year', b.created) = 2015
+        WHERE f.verified IS TRUE
 
     """,
 
@@ -26,7 +26,7 @@ QUERIES = {
         FROM bookoya_booking b
         JOIN bookoya_facility f ON f.id = b.facility_id
         INNER JOIN bookoya_monthlysubscription s ON b.facility_id = s.facility_id
-        WHERE f.verified IS TRUE AND date_part('year', b.created) = 2015
+        WHERE f.verified IS TRUE
 
     """,
 
